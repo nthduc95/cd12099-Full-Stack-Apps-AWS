be6b6e109e7d323839bbd258e819bc0e2855d80a
nthduc95 <nthduc95@gmail.com>
2023-08-01 17:29:36 +0700

    implement delete local file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { query, validationResult } from 'express-validator';
-import { filterImageFromURL } from './util/util.js';
+import { filterImageFromURL, deleteLocalFiles } from './util/util.js';
 
 
 
@@ -13,12 +13,18 @@ import { filterImageFromURL } from './util/util.js';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+ 
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async (req, res) => {
+    return res.status(200).send('Hello world!!!'); 
+  } );
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -27,23 +33,24 @@ import { filterImageFromURL } from './util/util.js';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-    /**************************************************************************** */
-
-  //! END @TODO1
-  
-  
-  // Root Endpoint
-  // Displays a simple message to the user
   app.get( "/filteredimage", query('image_url').notEmpty().isURL({protocols: ['http','https','ftp']}), async (req, res) => {
     const result = validationResult(req);
+
+    // validate the image_url query
     if (result.errors.length > 0) {
       return res.status(400).send(result.errors[0].msg);
     } 
-    const response = await filterImageFromURL(req.query.image_url);
-    return res.status(200).send(response); 
-  } );
-  
+    // call filterImageFromURL(image_url) to filter the image
+    filterImageFromURL(req.query.image_url).then((resolve) => {
+      // send the resulting file in the response
+      return res.status(200).sendFile(resolve)
+    }).finally((resolve) => {
+      if (resolve) {
+        // deletes any files on the server on finish of the response
+        deleteLocalFiles([resolve]);
+      }
+    });
+  });
 
   // Start the Server
   app.listen( port, () => {
